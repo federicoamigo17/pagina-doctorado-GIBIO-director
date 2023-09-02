826ff9dd3a9d8e9d4454b95e81a93c2c52e7a166
federicoamigo17 <133051821+federicoamigo17@users.noreply.github.com>
2023-09-01 21:03:29 -0300

    Add files via upload
    primer commit codigo funcional

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import InformeAnualForm from './formularioinformeanual';
+
+function App() {
+  return (
+    <div className="App">
+      <InformeAnualForm />
+    </div>
+  );
+}
+
+export default App;
